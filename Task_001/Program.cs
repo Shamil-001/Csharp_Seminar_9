b3ff121546d0d92f45f664ee0d239d927122d1b9
Shamil Magomedov <shmagomedov1997@mail.ru>
2023-07-17 21:48:44 +0300

    addFirstCommit

---
+++ Task_001/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNum(int firstNum, int secondNum)
+{
+    int sum = 0;
+    for (int i = firstNum; i <= secondNum; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int firstNum = ReadInt("Введите первое число: ");
+int secondNum = ReadInt("Введите второе число: ");
+int sum = SumNum(firstNum, secondNum);
+
+Console.WriteLine("Сумма натуральных элементов от {0} до {1}: {2}", firstNum, secondNum, sum);
---
+++ Task_002/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Ackermann(int firstNum, int secondNum)
+{
+    if (firstNum == 0)
+        return secondNum + 1;
+
+    if (secondNum == 0)
+        return Ackermann(firstNum - 1, 1);
+
+    return Ackermann(firstNum - 1, Ackermann(firstNum, secondNum - 1));
+}
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int firstNum = ReadInt("Введите первое значение: ");
+int secondNum = ReadInt("Введите второе значение: ");
+
+int result = Ackermann(firstNum, secondNum);
+
+Console.WriteLine($"Результат: {result}");
